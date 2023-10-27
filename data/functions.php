a6162fe51a7904587b6c4b2af82a001b7728ff7a
Idedav <xdavide.correa@gmail.com>
2023-10-27 17:35:21 +0200

    [add] password control

--- data/functions.php
+++ data/functions.php
@@ -7,11 +7,22 @@
             '0123456789',
             '@#_-.+?!$%&'
         ];
-        for ($i = 0; $i < $password_length; $i++){
-            $npos = rand(0, count($characters) - 1);
-            $n = rand(0, strlen($characters[$npos]) - 1);
-            $new_password .= $characters[$npos][$n];
-        }
+        
+        do{
+            $new_password = '';
+            for ($i = 0; $i < $password_length; $i++){
+                $npos = rand(0, count($characters) - 1);
+                $n = rand(0, strlen($characters[$npos]) - 1);
+                $new_password .= $characters[$npos][$n];
+            }
+        }while( similar_text($new_password , $characters[0]) > 0 
+                && 
+                similar_text($new_password , $characters[1]) > 0 
+                && 
+                similar_text($new_password , $characters[2]) > 0 
+                && 
+                similar_text($new_password , $characters[3]) > 0);
+
         return $new_password;
     }
 ?>
